8c6a7e86157db097e24a21fc0a97473dace6ac93
vikki057 <165292890+vikki057@users.noreply.github.com>
2024-07-08 23:38:33 +0530

    Add files via upload

---
+++ array.js
@@ -0,0 +1,44 @@
+// var a= ["vivek","aayush0","tigdi"]
+// a.forEach(function(index,value){
+//     console.log(index,value);
+// })
+
+// a=["vivek","aayush",23]
+// b=["viki",23,"alu"]
+// c=a.concat(b)
+// console.log(c);
+
+// var person=new Object();
+// person.firstname="vivek";
+// person.lastname="gururani";
+// person.age=18;
+// console.log(person);
+
+// var alu=[{name:"aayush",age:13},
+// {name:"vivek",age:18},
+// {name:"hari",age:15}]
+// console.log(alu)
+
+// const a={
+//     firstname:"vivek",
+//     lastname:"gururani",
+//     age:18
+// }
+// console.log(a.firstname);
+
+// for( var b in a){
+//     console.log(a[b]);
+// }
+
+// let a="vivek"
+// var b=a.length
+// console.log(b);
+
+// console.log( a.toUpperCase())
+// console.log( a.toLowerCase())
+
+// let str="aayush is a stupid boy"
+// console.log(str.includes("aayush"));
+// console.log(str.includes("stupid"));
+// console.log(str.includes("vivek"));
+
---
+++ vivek.js
@@ -0,0 +1,30 @@
+for(a=1;a<=100;a++){
+    if(a==6){
+        console.log("aayush is ", a, "<br>")
+        break;
+    }
+    console.log("number:",a,"<br>")
+}
+//     var ary=new Array()
+//     for(a=0;a<3;a++){
+//         ary[a]=prompt(("enter the value :"))
+//     }
+//     for(b=0;b<3;b++){
+//         document.write(ary[b])
+//     }
+
+// let a = [
+//     ["vivek" ,18 ,"<br>"],
+//     ["ayush" ,6  ,"<br>"]
+// ]
+// document.write(a)
+
+
+// b=["vivek","aayush","Sanjay"]
+// b.sort()  
+// b.reverse()
+// b.push(6)
+// b.pop()
+// console.log(b);
+
+
